533c8865b1cd17455db7f8200fe4a7217404afa7
Bebo41 <dokabanoub@gmail.com>
2023-05-19 14:44:17 +0300

    made javascript

--- Work/script.js
+++ Work/script.js
@@ -1,1 +1,9 @@
 document.write("This is also a test");
+var para = document.getElementById("par");
+var button = document.getElementById("button1");
+var abutton = document.getElementById("button2");
+function sure (){
+    para.innerHTML = "Ok";
+}
+button.addEventListener("click", sure)
+abutton.addEventListener("click", sure)
